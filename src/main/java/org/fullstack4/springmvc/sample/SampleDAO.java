21732d24790c88a5f84b776b808eb9e9bcd93338
Geeehyun <wkdwl578@gmail.com>
2024-04-15 17:03:17 +0900

    Initial commit

---
+++ src/main/java/org/fullstack4/springmvc/sample/SampleDAO.java
@@ -0,0 +1,11 @@
+package org.fullstack4.springmvc.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SampleDAO {
+}
