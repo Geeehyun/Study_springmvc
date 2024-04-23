dcc9d62f23fb82f1beb6392a2261535671d3c2e9
Geeehyun <wkdwl578@gmail.com>
2024-04-23 09:15:10 +0900

    study : 2024-04-23 실습 - Spring mvc favicon 에러 처리용 메서드 추가  ```  @GetMapping("favicon.ico")      @ResponseBody      public void returnNoFavicon() {          // Spring mvc favicon 에러 처리용      }  ```

--- src/main/java/org/fullstack4/springmvc/controller/SampleController.java
+++ src/main/java/org/fullstack4/springmvc/controller/SampleController.java
@@ -3,6 +3,7 @@ package org.fullstack4.springmvc.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,5 +18,10 @@ public class SampleController {
         log.info("---------------------");
 
         req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
+        // Spring mvc favicon 에러 처리용
     }
 }
